ad51fc5f70581f6b20faa5818ea8a4fd0fdf9bbb
Aaron Wilson <adub65@gmail.com>
2020-07-06 14:45:44 -0400

    basic loops in index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, message) {
+  let array = []
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`)
+  }
+  return array
+}
+
+function countDown(i) {
+  while (i >= 0) {
+    console.log(i)
+    i--
+  }
+}
